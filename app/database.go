c5228b6fea2f22022acc036f14ebb4116f0d6c78
Arya <aryaashari100@gmail.com>
2023-02-17 23:26:37 +0700

    create helper for panic err

--- app/database.go
+++ app/database.go
@@ -2,14 +2,13 @@ package app
 
 import (
 	"database/sql"
+	"ecourse-app/helper"
 	"time"
 )
 
 func GetNewConnection() *sql.DB {
 	db, err := sql.Open("mysql", "root@tcp(localhost:3306)/ecourse_app")
-	if err != nil {
-		panic(err)
-	}
+	helper.PanicError(&err)
 
 	db.SetMaxIdleConns(10)
 	db.SetMaxOpenConns(20)
